c0c9eb4fb5399948ca9b1d26c17e5b288e654dcf
gabriel <furstenheim@gmail.com>
2016-07-23 11:44:57 +0200

    Remove babelrc from test folder

--- test/main.js
+++ test/main.js
@@ -10,9 +10,21 @@ var plugin = require('../dist/index').default
 var tests = [{file: 'test1'}, {file: 'map'}, {file: 'nonDefaultLogger'}, {file: 'nonDefaultLogger2'}]/*.slice(3,4)*/
 describe('transform code', function () {
     tests.forEach(function(test){
-        it(`Only one preset ${test.file}`, function(done) {
+        it(`No preset ${test.file}`, function(done) {
             var transform = transformFileSync(path.join(__dirname, `src/${test.file}.js`), {
-                //'plugins': [plugin],
+                plugins: [plugin],
+                //presets : ['es2015']
+            }).code
+            var expected = fs.readFileSync(path.join(__dirname, `expected/${test.file}.js`)).toString()
+            console.log(diff.diffChars(transform, expected))
+            assert.equal(transform, expected)
+            done()
+        })
+    })
+    tests.forEach(function(test){
+        it(`One preset ${test.file}`, function(done) {
+            var transform = transformFileSync(path.join(__dirname, `src/${test.file}.js`), {
+                plugins: [plugin],
                 presets : ['es2015']
             }).code
             var expected = fs.readFileSync(path.join(__dirname, `expected/${test.file}.js`)).toString()
