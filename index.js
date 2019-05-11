5f55993837883022331d1a6cdf9bb98cc526752d
gabriel <furstenheim@gmail.com>
2019-05-11 20:25:43 +0200

    Add eslint config

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 var _ = require('lodash')
-export default function ({types: t}) {
-    return {
-        visitor  : {
-            CallExpression(path, options) {
-                var loggers = options.opts.loggers || [{pattern: 'console'}]
-                if (isLogger(path, loggers)) {
-                    var description = []
-                    for (let expression of path.node.arguments) {
-                        if (description.length === 0) {
-                            let relativePath
-                            let filePath = this.file.log.filename
-                            if (filePath.charAt(0) !== '/') {
-                                relativePath = filePath
-                            } else {
-                                let cwd = process.cwd()
-                                relativePath = filePath.substring(cwd.length + 1)
-                            }
+export default function ({ types: t }) {
+  return {
+    visitor: {
+      CallExpression (path, options) {
+        var loggers = options.opts.loggers || [ { pattern: 'console' } ]
+        if (isLogger(path, loggers)) {
+          var description = []
+          for (let expression of path.node.arguments) {
+            if (description.length === 0) {
+              let relativePath
+              let filePath = this.file.log.filename
+              if (filePath.charAt(0) !== '/') {
+                relativePath = filePath
+              } else {
+                let cwd = process.cwd()
+                relativePath = filePath.substring(cwd.length + 1)
+              }
 
-                            let line = expression.loc.start.line
-                            let column = expression.loc.start.column
-                            description.push(`${relativePath}:${line}:${column}:${this.file.code.substring(expression.start, expression.end)}`)
-                        } else {
-                            description.push(this.file.code.substring(expression.start, expression.end))
-                        }
-
-                    }
-                    path.node.arguments.unshift(t.stringLiteral(description.join(',')))
-                }
+              let line = expression.loc.start.line
+              let column = expression.loc.start.column
+              description.push(`${relativePath}:${line}:${column}:${this.file.code.substring(expression.start, expression.end)}`)
+            } else {
+              description.push(this.file.code.substring(expression.start, expression.end))
             }
+          }
+          path.node.arguments.unshift(t.stringLiteral(description.join(',')))
         }
+      }
     }
+  }
 
 }
 
-function isLogger(path, loggers) {
-    return _.some(loggers, function(logger) {
-        return path.get("callee").matchesPattern(logger.pattern, true)
-    })
+function isLogger (path, loggers) {
+  return _.some(loggers, function (logger) {
+    return path.get('callee').matchesPattern(logger.pattern, true)
+  })
 }
--- test/main.js
+++ test/main.js
@@ -1,41 +1,39 @@
-"use strict"
-var transformFileSync = require('babel-core').transformFileSync
-var path = require('path')
-var fs = require('fs')
-var assert = require('assert')
+const transformFileSync = require('@babel/core').transformFileSync
+const path = require('path')
+const fs = require('fs')
+const assert = require('assert')
 
-var plugin = require('../dist/index').default
+const plugin = require('../dist/index').default
 
-var tests = [{file: 'test1'},
-    {file: 'map'},
-    {file: 'nonDefaultLogger'},
-    {file: 'nonDefaultLogger2'},
-    {file: 'test1', options: {loggers : [{pattern: 'console'}]}},
-    {file: 'readLoggerFromOptions', options: {loggers: [{pattern: 'winston'}]}}
+const tests = [ { file: 'test1' },
+  { file: 'map' },
+  { file: 'nonDefaultLogger' },
+  { file: 'nonDefaultLogger2' },
+  { file: 'test1', options: { loggers: [ { pattern: 'console' } ] } },
+  { file: 'readLoggerFromOptions', options: { loggers: [ { pattern: 'winston' } ] } }
 ]
 describe('transform code', function () {
-    tests.forEach(function(test){
-        it(`No preset ${test.file}`, function(done) {
-            var transform = transformFileSync(`test/src/${test.file}.js`, {
-                plugins: [[plugin, test.options]],
-                babelrc: false // So we don't get babelrc from whole project
-            }).code
-            var expected = fs.readFileSync(path.join(__dirname, `expected/${test.file}.js`)).toString()
-            assert.equal(transform, expected)
-            done()
-        })
+  tests.forEach(function (test) {
+    it(`No preset ${test.file}`, function (done) {
+      const transform = transformFileSync(`test/src/${test.file}.js`, {
+        plugins: [ [ plugin, test.options ] ],
+        babelrc: false // So we don't get babelrc from whole project
+      }).code
+      const expected = fs.readFileSync(path.join(__dirname, `expected/${test.file}.js`)).toString()
+      assert.equal(transform, expected)
+      done()
     })
-    tests.forEach(function(test){
-        it(`Absolute path ${test.file}`, function(done) {
-            var transform = transformFileSync(path.join(__dirname, `src/${test.file}.js`), {
-                plugins: [[plugin, test.options]],
-                babelrc: false // So we don't get babelrc from whole project
-            }).code
-            var expected = fs.readFileSync(path.join(__dirname, `expected/${test.file}.js`)).toString()
-            assert.equal(transform, expected)
-            done()
-        })
+  })
+  tests.forEach(function (test) {
+    it(`Absolute path ${test.file}`, function (done) {
+      const transform = transformFileSync(path.join(__dirname, `src/${test.file}.js`), {
+        plugins: [ [ plugin, test.options ] ],
+        babelrc: false // So we don't get babelrc from whole project
+      }).code
+      const expected = fs.readFileSync(path.join(__dirname, `expected/${test.file}.js`)).toString()
+      assert.equal(transform, expected)
+      done()
     })
-
+  })
 })
 
